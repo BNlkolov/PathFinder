c34d39878d5fc3bfe28b4d7ab02df02cf2bbf90f
Borislav Nikolov <borislavn441@gmail.com>
2023-09-18 19:57:06 +0300

    fix

--- src/main/java/bg/softuni/pathfinder/web/RegisterController.java
+++ src/main/java/bg/softuni/pathfinder/web/RegisterController.java
@@ -1,15 +1,25 @@
 package bg.softuni.pathfinder.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import bg.softuni.pathfinder.model.dto.UserRegistrationDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
-        @GetMapping("/register")
-        public String register(){
-            return "register";
-        }
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String doRegister(UserRegistrationDTO userRegistrationDTO) {
+        System.out.println(userRegistrationDTO.toString());
+        return "redirect:/login";
+
+    }
+
 
 }
 
